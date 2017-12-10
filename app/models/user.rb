8bea1f8890db656d105146b5c14cc0968406ae95
Andrew Ying <hi@andrewying.com>
2017-12-10 11:11:26 +0800

    Update authentication settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :rememberable, :trackable, :lockable, :timeoutable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :lockable, :validatable
 
   has_and_belongs_to_many :tenants
   has_and_belongs_to_many :landlords
--- db/migrate/20171129212417_create_users.rb
+++ db/migrate/20171129212417_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string   :first_name
       t.string   :last_name
-      t.string   :email, null: false, default: ""
+      t.string   :username
+      t.string   :email
       t.string   :encrypted_password, null: false, default: ""
       t.datetime :remember_created_at
       t.integer  :sign_in_count, null: false, default: 0
@@ -16,6 +17,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.timestamps null: false
     end
 
+    add_index :users, :username, unique: true
     add_index :users, :email, unique: true
   end
 end
