9a0060d81b316a85171c5ff6ffa865c716349e4b
Andrew Ying <hi@andrewying.com>
2018-01-03 21:18:48 +0800

    Use hashes to improve ease of reading

--- app/jobs/generate_invoice_job.rb
+++ app/jobs/generate_invoice_job.rb
@@ -30,8 +30,9 @@ class GenerateInvoiceJob < ApplicationJob
   #
   # @return void
   def perform(tenancy, payment_date)
-    tenant = tenancy.tenant
-    invoice = tenant.invoices.new(
+    @tenancy = tenancy
+    tenant = @tenancy.tenant
+    @invoice = tenant.invoices.new(
       issued: true,
       issued_on: Date.current,
       due_on: payment_date,
@@ -40,105 +41,115 @@ class GenerateInvoiceJob < ApplicationJob
 
     # Associate pending transactions
     tenant.transactions.where('invoice_id IS NULL AND processed = true AND failed = false').each do |t|
-      t.invoice = invoice
+      t.invoice = @invoice
     end
 
     # Add rent to invoice
-    associate_rent_charges(invoice, tenancy, tenancy.rent_charges.order(to_date: desc).first.to_date.tomorrow,
-                           payment_date.yesterday)
+    associate_rent_charges({
+      from: @tenancy.rent_charges.order(to_date: desc).first.to_date.tomorrow,
+      to: payment_date.yesterday
+    })
 
     # Check whether new utilities charges have been added since the last invoice
     last_invoice = tenant.invoices.order(issued_at: :desc).first
-    associate_utility_charges(invoice, tenancy, last_invoice.empty? ? nil : last_invoice.issued_at)
+    associate_utility_charges(last_invoice.empty? ? nil : last_invoice.issued_at)
 
     starting_balance = last_invoice.balance
-    invoice.balance = starting_balance + invoice.transactions.sum('amount')
-    invoice.save!
+    @invoice.balance = starting_balance + @invoice.transactions.sum('amount')
+    @invoice.save!
 
-    file = generate_pdf_invoice(invoice, starting_balance)
-    InvoiceMailer.invoice_created(tenant, invoice, {
+    file = generate_pdf_invoice(starting_balance)
+    InvoiceMailer.invoice_created(tenant, @invoice, {
         filename: Date.current.strftime('Invoice-%d-%m-%Y.pdf'),
         content: file
     }).deliver_later
   end
 
+  private
+
   # Create and associate rent charges to the invoice.
   #
-  # @param [Invoice] invoice the invoice for the transactions to be associated to
-  # @param [Tenancy] tenancy the tenancy for which rent charges need to be created
-  # @param [Date] from the date from which rent need to be calculated
-  # @param [Date] to the date to which rent to be calculated
+  # @param [Hash] params the parameters of the charges to be created
   #
   # @return void
-  def associate_rent_charges(invoice, tenancy, from, to)
-    if tenancy.rent_payment_period == 'm'
+  def associate_rent_charges(params)
+    from = params.from
+    to = params.to
+
+    if @tenancy.rent_payment_period == 'm'
       # Calculate the end date assuming a full month of rent period
       current_to = from.next_month.yesterday
 
       # If a full month of rent is due
       while to >= current_to do
-        associate_rent_charge(invoice, tenancy, from, current_to, tenancy.rent)
+        associate_rent_charge({
+          from: from,
+          to: current_to,
+          amount: @tenancy.rent
+        })
 
         from = from.next_month
         current_to = from.next_month.yesterday
       end
 
       # If a partial month remains
-      associate_rent_charge(invoice, tenancy, from, current_to, tenancy.rent * (to - from).days / 30) if to > from
+      associate_rent_charge({
+        from: from,
+        to: to,
+        amount: @tenancy.rent * (to - from).days / 30
+      }) if to > from
     else
-      associate_rent_charge(invoice, tenancy, from, to, tenancy.rent * (to - from).days / 7)
+      associate_rent_charge({
+        from: from,
+        to: to,
+        amount: @tenancy.rent * (to - from).days / 7
+      })
     end
   end
 
   # Create and associate a single rent charge to the invoice.
   #
-  # @param [Invoice] invoice the invoice for the transactions to be associated to
-  # @param [Tenancy] tenancy the tenancy for which rent charge concerns
-  # @param [Date] from the date from which rent charge concerns
-  # @param [Date] to the date to which rent charge concerns
-  # @param [Integer|Float] amount amount of rent charge
+  # @param [Hash] params the parameters of the rent charge
   #
   # @return void
-  def associate_rent_charge(invoice, tenancy, from, to, amount)
-    rent_charge = tenancy.rent_charges.create!(
-        from_date: from,
-        to_date: to,
-        amount: amount
+  def associate_rent_charge(params)
+    rent_charge = @tenancy.rent_charges.create!(
+        from_date: params.from,
+        to_date: params.to,
+        amount: params.amount
     )
-    invoice.transactions.build(
+    @invoice.transactions.build(
         credit_date: Date.current,
-        description: 'Rent from ' + from.to_s(:rfc822) + 'to' + to.to_s(:rfc822),
-        amount: amount,
-        tenancy: tenancy,
+        description: 'Rent from ' + params.from.to_s(:rfc822) + 'to' + params.to.to_s(:rfc822),
+        amount: params.amount,
+        tenancy: @tenancy,
         transactionable: rent_charge
     )
   end
 
   # Associate utility charges billed from the `from` date
   #
-  # @param [Invoice] invoice the invoice for the transactions to be associated to
-  # @param [Tenancy] tenancy the tenancy concerned
   # @param [Date] from utility charges created after this date would be searched for:
   #   if nil utility charges created after the start of the tenancy would be searched
   #   for instead.
-  def associate_utility_charges(invoice, tenancy, *from)
-    tenancy.property.utilities.each do |utility|
+  def associate_utility_charges(*from)
+    @tenancy.property.utilities.each do |utility|
       if @from.nil?
         # Look for charges for usages after the start of tenancy
         charges = utility.utility_charges.where('usage_to_date =< ? AND usage_from_date >= ?', Date.current,
-                                                tenancy.start_date)
+                                                @tenancy.start_date)
       else
         charges = utility.utility_charges.where('usage_to_date =< ? AND usage_to_date > ?', Date.current,
                                                 from)
       end
 
       charges.each do |charge|
-        invoice.transactions.build(
+        @invoice.transactions.build(
             credit_date: Date.current,
             description: utility.name + ' charges from ' + charge.usage_from_date.to_s(:rfc822) + ' to ' +
                 charge.usage_to_date.to_s(:rfc822),
             amount: charge.amount,
-            tenancy: tenancy,
+            tenancy: @tenancy,
             transactionable: charge
         )
       end
@@ -147,15 +158,14 @@ class GenerateInvoiceJob < ApplicationJob
 
   # Generate PDF version of the invoice
   #
-  # @param [Invoice] invoice The invoice instance
   # @param [Integer|Float] starting_balance The balance carried forward
   #
   # @return String
-  def generate_pdf_invoice(invoice, starting_balance)
+  def generate_pdf_invoice(starting_balance)
     pdf = PDFKit.new(
-        render_to_string 'layouts/invoice', invoice: invoice, starting_balance: starting_balance
+        render_to_string 'layouts/invoice', invoice: @invoice, starting_balance: starting_balance
     )
-    invoice.documents.create!(
+    @invoice.documents.create!(
         name: Date.current.strftime('Invoice-%d-%m-%Y.pdf'),
         file: pdf.to_pdf,
         encrypted: false
